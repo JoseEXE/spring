ff24cb1fb0aca7ae37a60270f5f175f1348a954b
José AQUINO <angelexe8@hotmail.com>
2023-04-27 17:30:57 +0200

    d

--- src/main/java/fr/afpa/employes/controller/EmployesController.java
+++ src/main/java/fr/afpa/employes/controller/EmployesController.java
@@ -1,14 +1,18 @@
 package fr.afpa.employes.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.afpa.employes.Repository.EmployesRepository;
 import fr.afpa.employes.bean.Employes;
 import fr.afpa.employes.service.EmployesService;
 import jakarta.validation.Valid;
@@ -20,15 +24,32 @@ public class EmployesController {
 	@Autowired
 	private EmployesService employesService;
 	
+	@Autowired
+	private EmployesRepository employesRepository;
+	
+	
 	@GetMapping("/")
 	public String employes(Model model, Employes employes) {
 
 		model.addAttribute("employesList", employesService.showEmployes());
 		return "employes";
 	}
-	
 
-    //
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") int id, Model model, Employes employes) {
+
+		Optional<Employes> getEmploye = employesRepository.findById(id);
+		
+		Employes ami =null;
+		
+		if(getEmploye.isPresent()) {
+			ami = getEmploye.get();
+			model.addAttribute("amis",ami);
+		}
+		
+		return "employes";
+	}
+	
 	
 	@PostMapping("/")
 	public String employes(@Valid Employes employes, BindingResult bindingResult, Model model) {
